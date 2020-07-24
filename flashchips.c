8a07d9a6ece3c30a5b3b7dcd29d3bbde3cef6654
Timothy Lee <timothy.ty.lee@gmail.com>
2020-07-24 14:13:08 +1000

    flashchips: Add Adesto AT25DF041B
    This commit adds support for the Adesto AT25DF041B SPI flash chip, which is
    similar to AT25DF041A, except for a different JEDEC device ID and a wider
    voltage range.  See:
    https://www.xcore.com/viewtopic.php?t=4716
    
    Datasheet is available from:
    http://www.adestotech.com/wp-content/uploads/DS-AT25DF041B_040.pdf
    
    Signed-off-by: Timothy Lee <timothy.ty.lee@gmail.com>

--- flashchips.c
+++ flashchips.c
@@ -1600,6 +1600,44 @@ const struct flashchip flashchips[] = {
 
 	{
 		.vendor		= "Atmel",
+		.name		= "AT25DF041B",
+		.bustype	= BUS_SPI,
+		.manufacture_id	= ATMEL_ID,
+		.model_id	= ATMEL_AT25DF041B,
+		.total_size	= 512,
+		.page_size	= 256,
+		.feature_bits	= FEATURE_WRSR_WREN,
+		.tested		= TEST_OK_PREW,
+		.probe		= probe_spi_rdid,
+		.probe_timing	= TIMING_ZERO,
+		.block_erasers	=
+		{
+			{
+				.eraseblocks = { {4 * 1024, 128} },
+				.block_erase = spi_block_erase_20,
+			}, {
+				.eraseblocks = { {32 * 1024, 16} },
+				.block_erase = spi_block_erase_52,
+			}, {
+				.eraseblocks = { {64 * 1024, 8} },
+				.block_erase = spi_block_erase_d8,
+			}, {
+				.eraseblocks = { {512 * 1024, 1} },
+				.block_erase = spi_block_erase_60,
+			}, {
+				.eraseblocks = { {512 * 1024, 1} },
+				.block_erase = spi_block_erase_c7,
+			}
+		},
+		.printlock	= spi_prettyprint_status_register_at25df,
+		.unlock		= spi_disable_blockprotect_at2x_global_unprotect,
+		.write		= spi_chip_write_256,
+		.read		= spi_chip_read,
+		.voltage	= {1650, 3600},
+	},
+
+	{
+		.vendor		= "Atmel",
 		.name		= "AT25DF081",
 		.bustype	= BUS_SPI,
 		.manufacture_id	= ATMEL_ID,
